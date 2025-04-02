a6f96a257562e208906164e0ae5be79790366cf6
rreiter37 <robinreiter61@gmail.com>
2025-04-02 17:12:10 +0200

    test commit

---
+++ phd_start.py
@@ -0,0 +1,11 @@
+### Prise en main des fonctionnalités du package Pinard
+
+### Importation des bibliothèques nécessaires
+
+import numpy as np
+import pinard as pn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+### Importation des données
+
